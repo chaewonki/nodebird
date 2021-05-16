c8ca32de6a9b979d7ec865df8a0c15efe52c5100
rivmt <chaewk15@gmail.com>
2021-05-16 17:26:26 +0900

    Edir Profile2
    changePassword

--- routes/user.js
+++ routes/user.js
@@ -51,11 +51,33 @@ router.post("/editProfile", isLoggedIn, async (req, res, next) => {
 });
 
 router.get("/changePassword", isLoggedIn, (req, res) => {
-  res.render("changePassword", { title: "비밀번호 변경", user: req.user });
+  res.render("changePassword", {
+    title: "비밀번호 변경 - NodeBird",
+    user: req.user,
+    passwordError: req.flash("passwordError"),
+  });
 });
 
 router.post("/changePassword", isLoggedIn, async (req, res, next) => {
   try {
+    const {
+      body: { oldPassword, newPassword1, newPassword2 },
+    } = req;
+    const user = await User.findOne({ where: { id: req.user.id } });
+    const result = await bcrypt.compare(oldPassword, user.password);
+    if (result) {
+      if (newPassword1 === newPassword2) {
+        const hash = await bcrypt.hash(newPassword1, 12);
+        user.update({ password: hash });
+        res.redirect("/");
+      } else {
+        req.flash("passwordError", "새 패스워드는 동일해야 합니다.");
+        return res.redirect("/user/changePassword");
+      }
+    } else {
+      req.flash("passwordError", "기존 패스워드와 동일한 값이 아닙니다.");
+      return res.redirect("/user/changePassword");
+    }
   } catch (error) {
     console.error(error);
     next(error);
